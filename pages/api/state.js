224dff33b31ec991f362d44c2263095d1257d764
AdrianAndersen <adrianandersen@protonmail.com>
2021-10-01 21:28:27 +0200

    Refactor bid aggregation into separate method

--- pages/api/state.js
+++ pages/api/state.js
@@ -13,7 +13,49 @@ const username = process.env.DATABASE_USER;
 const password = process.env.DATABASE_PASSWORD;
 const dbname = "Charity";
 
+const getHighestBids = () =>
+  Bid.aggregate([
+    // Find the highest bids for each item
+    {
+      $group: {
+        _id: "$item",
+        amount: {
+          $max: "$amount",
+        },
+      },
+    },
+    // JOIN with auction table to get item description
+    {
+      $lookup: {
+        from: "auctions",
+        localField: "_id",
+        foreignField: "id",
+        as: "auctions",
+      },
+    },
+    // Destructure auction array, since each bid only has one auctioned item
+    {
+      $unwind: "$auctions",
+    },
+    // Format the data
+    {
+      $project: {
+        price: "$amount",
+        description: "$auctions.description",
+        id: "$auctions.id",
+        _id: 0,
+      },
+    },
+    // Make sure the bids are in the same order every time
+    {
+      $sort: {
+        id: 1,
+      },
+    },
+  ]);
+
 export const url = `mongodb+srv://${username}:${password}@cluster.au8e8.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+
 export default async function handler(_, res) {
   mongoose.connect(url);
 
@@ -25,40 +67,7 @@ export default async function handler(_, res) {
       SlidoView.findOne().sort({ date: -1 }).limit(1),
       StretchGoal.find({}).sort("goal"),
       Vipps.findOne({}).sort({ amount: -1 }).limit(1),
-      Bid.aggregate([
-        {
-          $group: {
-            _id: "$item",
-            amount: {
-              $max: "$amount",
-            },
-          },
-        },
-        {
-          $lookup: {
-            from: "auctions",
-            localField: "_id",
-            foreignField: "id",
-            as: "auctions",
-          },
-        },
-        {
-          $unwind: "$auctions",
-        },
-        {
-          $project: {
-            price: "$amount",
-            description: "$auctions.description",
-            id: "$auctions.id",
-            _id: 0,
-          },
-        },
-        {
-          $sort: {
-            id: 1,
-          },
-        },
-      ]),
+      getHighestBids(),
     ]);
 
   res.statusCode = 200;
