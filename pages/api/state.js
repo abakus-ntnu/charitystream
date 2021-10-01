23a354684359c3240febb39cd1b0765d310d248d
AdrianAndersen <adrianandersen@protonmail.com>
2021-10-01 17:09:56 +0200

    Replace static prices with automatic bid filtering
    Aggregate the bids to find the current highest bids.
    This makes it easier to delete bids in the future,
    as you only need to remove a bid from the Bid table.

--- pages/api/state.js
+++ pages/api/state.js
@@ -6,6 +6,7 @@ import {
   StreamLink,
   SlidoView,
   StretchGoal,
+  Bid,
 } from "../../models/schema.js";
 
 const username = process.env.DATABASE_USER;
@@ -13,12 +14,39 @@ const password = process.env.DATABASE_PASSWORD;
 const dbname = "Charity";
 
 export const url = `mongodb+srv://${username}:${password}@cluster.au8e8.mongodb.net/${dbname}?retryWrites=true&w=majority`;
-
 export default async function handler(_, res) {
   mongoose.connect(url);
+  const auctions = await Bid.aggregate([
+    {
+      $group: {
+        _id: "$item",
+        amount: {
+          $max: "$amount",
+        },
+      },
+    },
+    {
+      $lookup: {
+        from: "auctions",
+        localField: "_id",
+        foreignField: "id",
+        as: "auctions",
+      },
+    },
+    {
+      $unwind: "$auctions",
+    },
+    {
+      $project: {
+        price: "$amount",
+        description: "$auctions.description",
+        id: "$auctions.id",
+        _id: 0,
+      },
+    },
+  ]);
 
   // Get all the state we need for the page
-  const auctions = await Auction.find({});
   const vipps = await Vipps.find({});
   const streamLink = await StreamLink.findOne().sort({ date: -1 }).limit(1);
   const slidoView = await SlidoView.findOne().sort({ date: -1 }).limit(1);
