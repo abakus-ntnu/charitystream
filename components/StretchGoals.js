af7656a7e0efea971cbd058fc5ac3b691e91b632
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2021-10-02 18:38:58 +0200

    Add total donated

--- components/StretchGoals.js
+++ components/StretchGoals.js
@@ -10,7 +10,7 @@ export default function StretchGoals(props) {
   return (
     <div className="flex flex-col flex-grow h-full">
       <div className="italic text-center">
-        <p className="text-3xl font-medium w-">Totalt innsamlet</p>
+        <p className="text-5xl font-medium w-">Totalt donert: {totalAmount}kr</p>
       </div>
       <div className="flex items-center flex-grow p-2 overflow-hidden">
         <div className="w-full h-full py-4">
