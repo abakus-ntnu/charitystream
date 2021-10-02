70d25dd16ca1a50a70daa81c9401e745ea6d15d6
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2021-10-02 13:53:24 +0200

    Add vipps page

---
+++ components/admin/SetPasswordBox.tsx
@@ -0,0 +1,18 @@
+import Alert from "./Alert";
+import Link from "next/link";
+
+const SetPasswordBox = ({action = "gjøre dette"}) => {
+  return (
+      <div className="flex items-center justify-between">
+        <Alert
+            header="Obs!"
+            text={`Du må sette et passord for å ${action}. Gå til forsiden`}
+        />
+        <div className="bg-gray-400 px-3 py-2 mx-5 rounded-md text-large font-medium text-black">
+          <Link href="/admin">Sett passord</Link>
+        </div>
+      </div>
+  )
+}
+
+export default SetPasswordBox;
