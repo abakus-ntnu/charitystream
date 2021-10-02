3d04782c6eeb91a3db59c6d85ce1ae9f8a5b3f60
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2021-10-02 18:36:52 +0200

    remove old code

--- components/StretchGoalBar.js
+++ components/StretchGoalBar.js
@@ -39,41 +39,4 @@ const StretchGoalBar = ({ totalAmount, maxAmount, stretchGoals }) => {
   );
 };
 
-{
-  /*
-  <div className="text-center">
-          <div className="text-2xl">Mål:</div>
-          <hr />
-          <table className="table-auto">
-            <tbody>
-              {nextGoals.map((item, i) => {
-                const color =
-                  i == nextGoals.length - 1
-                    ? "text-indigo-400 animate-pulse"
-                    : "";
-                return (
-                  <tr key={item.description}>
-                    <td className={`px-4 text-left text-lg ${color}`}>
-                      {item.description}
-                    </td>
-                    <td className={`px-4 ${color}`}>{item.goal} ,-</td>
-                  </tr>
-                );
-              })}
-              {reachedGoals.map((item) => (
-                <tr key={item.description}>
-                  <td className="px-4 line-through text-green-400 text-left text-lg">
-                    {item.description}
-                  </td>
-                  <td className="px-4 text-green-400 line-through">
-                    {item.goal} ,-
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-   */
-}
-
 export default StretchGoalBar;
