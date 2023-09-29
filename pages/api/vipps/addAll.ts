8926e4322d75a621e50c9364cdc20a9f8fef7b39
PeterJFB <peterj.flbj@gmail.com>
2023-09-29 18:17:03 +0200

    Prettier

--- pages/api/vipps/addAll.ts
+++ pages/api/vipps/addAll.ts
@@ -10,64 +10,64 @@ const cors = Cors();
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
 function runMiddleware(req, res, fn) {
-	return new Promise((resolve, reject) => {
-		fn(req, res, (result) => {
-			if (result instanceof Error) {
-				return reject(result);
-			}
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
 
-			return resolve(result);
-		});
-	});
+      return resolve(result);
+    });
+  });
 }
 
 export default async function handler(req, res) {
-	const { method, headers } = req;
+  const { method, headers } = req;
 
-	await runMiddleware(req, res, cors);
+  await runMiddleware(req, res, cors);
 
-	mongoose.connect(url);
+  mongoose.connect(url);
 
-	if (headers.password !== process.env.POST_PASSWORD) {
-		res.status(401).end();
-		return;
-	}
+  if (headers.password !== process.env.POST_PASSWORD) {
+    res.status(401).end();
+    return;
+  }
 
-	switch (method) {
-		case "POST":
-			const content: string = req.body;
+  switch (method) {
+    case "POST":
+      const content: string = req.body;
 
-			const rows = content.split("\n");
-			const header = rows.splice(0, 1);
+      const rows = content.split("\n");
+      const header = rows.splice(0, 1);
 
-			const operations: mongodb.AnyBulkWriteOperation<{
-				name: string;
-				amount: number;
-			}>[] = [{ deleteMany: { filter: {} } }];
+      const operations: mongodb.AnyBulkWriteOperation<{
+        name: string;
+        amount: number;
+      }>[] = [{ deleteMany: { filter: {} } }];
 
-			for (const row of rows) {
-				const [dateString, brutto, firstName, lastName] = row.split(",");
+      for (const row of rows) {
+        const [dateString, brutto, firstName, lastName] = row.split(",");
 
-				const vipps = new Vipps({
-					name: `${firstName} ${lastName}`,
-					amount: brutto,
-				});
+        const vipps = new Vipps({
+          name: `${firstName} ${lastName}`,
+          amount: brutto,
+        });
 
-				operations.push({
-					insertOne: {
-						document: vipps,
-					},
-				});
-				// await vipps.save();
-			}
+        operations.push({
+          insertOne: {
+            document: vipps,
+          },
+        });
+        // await vipps.save();
+      }
 
-			await Vipps.bulkWrite(operations);
+      await Vipps.bulkWrite(operations);
 
-			res.status(200).json(Vipps.length);
+      res.status(200).json(Vipps.length);
 
-			break;
-		default:
-			res.setHeader("Allow", ["POST"]);
-			res.status(405).end(`Method ${method} Not Allowed`);
-	}
+      break;
+    default:
+      res.setHeader("Allow", ["POST"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
 }
